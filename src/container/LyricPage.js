9989e9aa7dc387a4be28d96ee0a4ee8d97681be4
Joe Klause <joeklause@gmail.com>
2019-11-07 11:36:28 -0800

    lyricpage refactored

--- src/container/LyricPage.js
+++ src/container/LyricPage.js
@@ -1,45 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Lyrics from '../components/lyrics/Lyrics';
 import { getLyrics } from '../services/lyricsApi';
 import PropTypes from 'prop-types';
 
-export default class LyricPage extends Component {
+const LyricPage = ({ match }) => {
 
-  static propTypes = {
-    history: PropTypes.object.isRequired,
-    match: PropTypes.shape({
-      params: PropTypes.shape({
-        title: PropTypes.string,
-        artist: PropTypes.string,
-      })
-    })
-  }
+  const [lyrics, setLyrics] = useState('');
+  const [loading, setLoading] = useState(true);
 
-  state = {
-    lyrics: '',
-    loading: true
-  }
-
-  fetchLyrics = () => {
-    this.setState({ loading: true }); 
-    getLyrics(this.props.match.params.title, this.props.match.params.artist)
+  const fetchLyrics = () => {
+    setLoading(true); 
+    getLyrics(match.params.title, match.params.artist)
       .then((res) => {
-        this.setState({ lyrics: res.lyrics, loading: false });
+        setLyrics(res.lyrics);
+        setLoading(false);
       });
-  }
+  };
 
-  componentDidMount() {
-    this.fetchLyrics();
-  }
+  useEffect(() => {
+    fetchLyrics();
+  }, []);
 
-  render() {
-    if(this.state.loading) return <img src='https://loading.io/spinners/music/lg.music-note-preloader.gif'/>;
-    return (
-      <>
-        <Lyrics title={this.props.match.params.title} lyrics={this.state.lyrics} artist={this.props.match.params.artist} />
-      </>
-    );
 
-  }
+  if(loading) return <img src='https://loading.io/spinners/music/lg.music-note-preloader.gif'/>;
+  return (
+    <>
+      <Lyrics title={match.params.title} lyrics={lyrics} artist={match.params.artist} />
+    </>
+  );
 
-}
+};
+
+LyricPage.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      title: PropTypes.string,
+      artist: PropTypes.string,
+    }).isRequired
+  }).isRequired
+};
